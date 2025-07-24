5fe627a6158e92ff426942089645e71d4b4f101f
Romano Solis <soliswebsolutions@gmail.com>
2025-07-24 16:11:30 +0200

    skull king meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Skull King Scorekeeper',
+  description: 'Skull King Scorekeeper',
+  generator: 'Skull King Scorekeeper',
 }
 
 export default function RootLayout({
